4f9414d757cca6ee08c9312736a37e77c25908f5
מירה ברון <b8010040@gmail.com>
2025-03-30 16:03:39 +0300

    start project

---
+++ render-app/server.js
@@ -0,0 +1,30 @@
+
+
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/',(req,res)=>{
+    res.send('hello world!')
+});
+app.get('/services', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.render.com/v1/services?limit=20', {
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${process.env.RENDER_API_KEY}`
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching services:', error);
+        res.status(error.response ? error.response.status : 500).json({ message: 'Error fetching services' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
