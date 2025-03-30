e2c5b7afa3c0b07897c3625f212a1ef43d2dc859
מירה ברון <b8010040@gmail.com>
2025-03-30 22:46:33 +0300

    update code

--- render-app/server.js
+++ render-app/server.js
@@ -4,16 +4,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // API Key של Render
-const renderApiKey = 'rnd_tHKPhbHuNTrLEg5UXch4rmvTTfRu';  // החלף בזה את ה-API Key שלך
-app.get('/', (req, res) => {
-    res.send('Hello World!');  // או כל תוכן אחר שתרצי
-});
+// const renderApiKey = 'rnd_tHKPhbHuNTrLEg5UXch4rmvTTfRu';  // החלף בזה את ה-API Key שלך
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');  // או כל תוכן אחר שתרצי
+// });
 
-app.get('/apps', async (req, res) => {
+app.get('/', async (req, res) => {
     try {
         const response = await axios.get('https://api.render.com/v1/services', {
             headers: {
-                Authorization: `Bearer ${renderApiKey}`,
+                Authorization: `Bearer ${RENDER_API_KEY}`,
             },
         });
         res.json(response.data);  // מחזיר את רשימת האפליקציות ב-JSON
